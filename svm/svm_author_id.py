39a12bebc33ccd29eb848399558391604aa99be0
Bry Bartle <brybartle@gmail.com>
2017-03-28 19:17:47 -0400

    use kernel="rbf" and C values 10,100,1000,10000 (10000 gives best accuracy)

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -27,7 +27,7 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 # Import classifier SVC from sklearn.svm
 from sklearn.svm import SVC
-clf = SVC(kernel="linear")
+clf = SVC(C=10000., kernel="rbf")
 
 # Decreasing training dataset size
 features_train = features_train[:len(features_train)/100] 
