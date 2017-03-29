d00f4140c0cfd34e3ba68e2296196af3e59dc4b8
Bry Bartle <brybartle@gmail.com>
2017-03-29 10:04:57 -0400

    train and predict with DT at min_samples_split = 40

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -10,7 +10,7 @@
     
 import sys
 from time import time
-sys.path.append("../tools/")
+sys.path.append("C:/Users/Bryan/Documents/machine-learning/ud120-projects/tools/")
 from email_preprocess import preprocess
 
 
@@ -24,6 +24,19 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+
+# Import and train Decision Tree Classifier
+from sklearn.tree import DecisionTreeClassifier
+clf = DecisionTreeClassifier(min_samples_split = 40)
+clf.fit(features_train, labels_train)
+
+# Predict with trained DT Classifier
+labels_pred = clf.predict(features_test)
+
+# Measure accuracy of predictions relative to actual test labels
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(labels_pred, labels_test)
+print acc
 
 
 #########################################################
