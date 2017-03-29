2ca65733c4cdc7fba396d88c2676b165c4f48b3f
Bry Bartle <brybartle@gmail.com>
2017-03-28 20:00:36 -0400

    go back to full dataset and count # of Chris predictions

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -27,11 +27,7 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 # Import classifier SVC from sklearn.svm
 from sklearn.svm import SVC
-clf = SVC(C=10000., kernel="rbf")
-
-# Use smaller training dataset
-features_train = features_train[:len(features_train)/100] 
-labels_train = labels_train[:len(labels_train)/100]  
+clf = SVC(C=10000., kernel="rbf") 
 
 # Train (fit) classifier with training data
 t0 = time()
@@ -42,9 +38,11 @@ print "training time:", round(time()-t0, 3), "seconds"
 t1 = time()
 labels_pred = clf.predict(features_test)
 print "predicting time:", round(time()-t1, 3), "seconds"
-print "prediction for element 10 is", labels_pred[10]
-print "prediction for element 26 is", labels_pred[26]
-print "prediction for element 50 is", labels_pred[50]
+
+# Count how many predictions for Chris
+from collections import Counter
+Chris_pred = Counter(labels_pred)
+print Chris_pred
 
 # Import and use accuracy_score to measure accuracy
 from sklearn.metrics import accuracy_score
